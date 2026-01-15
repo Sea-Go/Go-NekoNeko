079869f0583f5a66a3773d562303fcf6889b49d9
unicornshjl <3159669749@qq.com>
2026-01-15 16:48:58 +0800

    feat(admin):管理员修改用户信息逻辑

--- service/admin/api/internal/logic/admin/updateuserlogic.go
+++ service/admin/api/internal/logic/admin/updateuserlogic.go
@@ -5,12 +5,10 @@ package admin
 
 import (
 	"context"
-	"errors"
 
-	"sea-try-go/service/admin/api/internal/model"
 	"sea-try-go/service/admin/api/internal/svc"
 	"sea-try-go/service/admin/api/internal/types"
-	"sea-try-go/service/common/cryptx"
+	"sea-try-go/service/admin/rpc/pb"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -30,47 +28,28 @@ func NewUpdateuserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Update
 }
 
 func (l *UpdateuserLogic) Updateuser(req *types.UpdateUserReq) (resp *types.UpdateUserResp, err error) {
-	user := model.User{}
-	id := req.Id
-	err = l.svcCtx.DB.Where("id = ?", id).First(&user).Error
-	if err != nil {
-		return nil, errors.New("用户不存在")
+
+	rpcReq := &pb.UpdateUserReq{
+		Id:        req.Id,
+		Username:  req.Username,
+		Password:  req.Password,
+		Email:     req.Email,
+		ExtraInfo: req.Extrainfo,
 	}
-	updates := make(map[string]interface{})
-	if len(req.Username) > 0 {
-		updates["username"] = req.Username
-	}
-	if len(req.Password) > 0 {
-		newPassword, e := cryptx.PasswordEncrypt(req.Password)
-		if e != nil {
-			return nil, errors.New("密码加密失败")
-		}
-		updates["password"] = newPassword
-	}
-	if len(req.Email) > 0 {
-		updates["email"] = req.Email
-	}
-	if req.Extrainfo != nil {
-		updates["extra_info"] = req.Extrainfo
-	}
-	if len(updates) > 0 {
-		err = l.svcCtx.DB.Model(&model.User{}).Where("id = ?", id).Updates(updates).Error
-		if err != nil {
-			return nil, errors.New("更新失败" + err.Error())
-		}
-	}
-	var newUser model.User
-	err = l.svcCtx.DB.Model(&model.User{}).Where("id = ?", id).First(&newUser).Error
+
+	rpcResp, err := l.svcCtx.AdminRpc.UpdateUser(l.ctx, rpcReq)
 	if err != nil {
 		return nil, err
 	}
+
 	return &types.UpdateUserResp{
 		Success: true,
 		User: types.UserInfo{
-			Id:        newUser.Id,
-			Username:  newUser.Username,
-			Email:     newUser.Email,
-			Extrainfo: newUser.ExtraInfo,
+			Id:        rpcResp.User.Id,
+			Username:  rpcResp.User.Username,
+			Email:     rpcResp.User.Email,
+			Status:    int64(rpcResp.User.Status),
+			Extrainfo: rpcResp.User.ExtraInfo,
 		},
 	}, nil
 }

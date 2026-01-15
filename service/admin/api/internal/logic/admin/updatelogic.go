62f02f2b6b810e12c5c236a70122a1ee3c157dc1
unicornshjl <3159669749@qq.com>
2026-01-15 16:56:09 +0800

    feat(admin):新增管理员更改信息逻辑

--- service/admin/api/internal/logic/admin/updatelogic.go
+++ service/admin/api/internal/logic/admin/updatelogic.go
@@ -8,10 +8,9 @@ import (
 	"encoding/json"
 	"errors"
 
-	"sea-try-go/service/admin/api/internal/model"
 	"sea-try-go/service/admin/api/internal/svc"
 	"sea-try-go/service/admin/api/internal/types"
-	"sea-try-go/service/common/cryptx"
+	"sea-try-go/service/admin/rpc/pb"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -35,38 +34,19 @@ func (l *UpdateLogic) Update(req *types.UpdateSelfReq) (resp *types.UpdateSelfRe
 	if !ok {
 		return nil, errors.New("Token 解析异常")
 	}
-	id, er := userId.Int64()
-	if er != nil {
-		return nil, er
+	id, err := userId.Int64()
+	if err != nil {
+		return nil, err
 	}
-	updates := make(map[string]interface{})
-	if len(req.Username) > 0 {
-		updates["username"] = req.Username
-	}
-
-	if len(req.Password) > 0 {
-		newPassword, e := cryptx.PasswordEncrypt(req.Password)
-		if e != nil {
-			return nil, e
-		}
-		updates["password"] = newPassword
+	rpcReq := &pb.UpdateSelfReq{
+		Id:        uint64(id),
+		Username:  req.Username,
+		Password:  req.Password,
+		Email:     req.Email,
+		ExtraInfo: req.Extrainfo,
 	}
 
-	if len(req.Email) > 0 {
-		updates["email"] = req.Email
-	}
-	if req.Extrainfo != nil {
-		updates["extra_info"] = req.Extrainfo
-	}
-
-	if len(updates) > 0 {
-		err = l.svcCtx.DB.Model(&model.Admin{}).Where("id = ?", id).Updates(updates).Error
-		if err != nil {
-			return nil, errors.New("更新失败:" + err.Error())
-		}
-	}
-	var newAdmin model.Admin
-	err = l.svcCtx.DB.Model(&model.Admin{}).Where("id = ?", id).First(&newAdmin).Error
+	rpcResp, err := l.svcCtx.AdminRpc.UpdateSelf(l.ctx, rpcReq)
 	if err != nil {
 		return nil, err
 	}
@@ -74,10 +54,10 @@ func (l *UpdateLogic) Update(req *types.UpdateSelfReq) (resp *types.UpdateSelfRe
 	return &types.UpdateSelfResp{
 		Success: true,
 		Admin: types.AdminInfo{
-			Id:        newAdmin.Id,
-			Username:  newAdmin.Username,
-			Email:     newAdmin.Email,
-			Extrainfo: newAdmin.ExtraInfo,
+			Id:        rpcResp.Admin.Id,
+			Username:  rpcResp.Admin.Username,
+			Email:     rpcResp.Admin.Email,
+			Extrainfo: rpcResp.Admin.ExtraInfo,
 		},
 	}, nil
 

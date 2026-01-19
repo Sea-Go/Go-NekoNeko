acaca44f6244b773e6595ed9b35f001d238643f1
senak1129 <1418172577@qq.com>
2026-01-19 22:44:40 +0800

    生成rpc

---
+++ service/article/rpc/internal/logic/list_articles_logic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"sea-try-go/service/article/rpc/internal/svc"
+	"sea-try-go/service/article/rpc/pb"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type ListArticlesLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewListArticlesLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ListArticlesLogic {
+	return &ListArticlesLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *ListArticlesLogic) ListArticles(in *__.ListArticlesRequest) (*__.ListArticlesResponse, error) {
+	// todo: add your logic here and delete this line
+
+	return &__.ListArticlesResponse{}, nil
+}

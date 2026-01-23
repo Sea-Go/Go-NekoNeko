0aa8017848bfe6fbea15e3d88f9d8f6cc8787d91
Hei-MaoM <2847944739@qq.com>
2026-01-23 21:07:27 +0800

    feat(points):添加数据库，kafka初始化

--- service/points/rpc/internal/config/config.go
+++ service/points/rpc/internal/config/config.go
@@ -4,4 +4,23 @@ import "github.com/zeromicro/go-zero/zrpc"
 
 type Config struct {
 	zrpc.RpcServerConf
+	Postgres struct {
+		Host     string
+		Port     string
+		User     string
+		Password string
+		DBName   string
+		Mode     string
+	}
+	Redis struct {
+		Addr     string
+		Password string
+		DB       int
+		PoolSize int
+	}
+	KafkaConf struct {
+		Brokers []string
+		Topic   string
+		Group   string
+	}
 }
--- service/points/rpc/internal/svc/service_context.go
+++ service/points/rpc/internal/svc/service_context.go
@@ -1,13 +1,49 @@
 package svc
 
-import "sea-try-go/service/points/rpc/internal/config"
+import (
+	"sea-try-go/service/points/rpc/internal/config"
+	"sea-try-go/service/points/rpc/internal/model"
+
+	"github.com/IBM/sarama"
+	"github.com/redis/go-redis/v9"
+)
 
 type ServiceContext struct {
-	Config config.Config
+	Config      config.Config
+	PointsModel *model.PointsModel
+	RDB         *redis.Client
+	KafKa       sarama.SyncProducer
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	dbConfig := model.DBConf{
+		Host:     c.Postgres.Host,
+		Port:     c.Postgres.Port,
+		User:     c.Postgres.User,
+		Password: c.Postgres.Password,
+		DBName:   c.Postgres.DBName,
+		Mode:     c.Postgres.Mode,
+	}
+	db := model.InitDB(dbConfig)
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     c.Redis.Addr,
+		Password: c.Redis.Password,
+		DB:       c.Redis.DB,
+		PoolSize: c.Redis.PoolSize,
+	})
+	kafka := sarama.NewConfig()
+	kafka.Producer.RequiredAcks = sarama.WaitForAll
+	kafka.Producer.Retry.Max = 5
+	kafka.Producer.Return.Successes = true
+	kafka.Producer.Idempotent = true
+	kfk, err := sarama.NewSyncProducer(c.KafkaConf.Brokers, kafka)
+	if err != nil {
+		panic("kafka连接失败:" + err.Error())
+	}
 	return &ServiceContext{
-		Config: c,
+		Config:      c,
+		PointsModel: model.NewPointsModel(db),
+		RDB:         rdb,
+		KafKa:       kfk,
 	}
 }

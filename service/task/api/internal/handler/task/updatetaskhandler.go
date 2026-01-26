37730eb5e7261d211bc1f09800cb5e342a9a5cb6
190y <1903228526@qq.com>
2026-01-26 16:43:36 +0800

    增加了task的api文件

---
+++ service/task/api/internal/handler/task/updatetaskhandler.go
@@ -0,0 +1,31 @@
+// Code scaffolded by goctl. Safe to edit.
+// goctl 1.9.2
+
+package task
+
+import (
+	"net/http"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"sea-try-go/service/task/api/internal/logic/task"
+	"sea-try-go/service/task/api/internal/svc"
+	"sea-try-go/service/task/api/internal/types"
+)
+
+func UpdateTaskHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.UpdateTaskReq
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := task.NewUpdateTaskLogic(r.Context(), svcCtx)
+		resp, err := l.UpdateTask(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}

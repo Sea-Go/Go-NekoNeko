9053ac12e0bf6821b2f0b3e980fcd9a83702f495
unicornshjl <3159669749@qq.com>
2026-01-27 22:14:11 +0800

    fix(user,admin):将user和admin归并到同一文件夹user下，同时调整了common部分的内容，将只属于user的内容移动到了user目录下

---
+++ service/user/admin/api/internal/handler/admin/updatehandler.go
@@ -0,0 +1,31 @@
+// Code scaffolded by goctl. Safe to edit.
+// goctl 1.9.2
+
+package admin
+
+import (
+	"net/http"
+	"sea-try-go/service/user/admin/api/internal/logic/admin"
+	"sea-try-go/service/user/admin/api/internal/svc"
+	"sea-try-go/service/user/admin/api/internal/types"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func UpdateHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.UpdateSelfReq
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := admin.NewUpdateLogic(r.Context(), svcCtx)
+		resp, err := l.Update(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
---
+++ service/user/admin/rpc/internal/logic/deleteuserlogic.go
@@ -0,0 +1,38 @@
+package logic
+
+import (
+	"context"
+	"errors"
+	"sea-try-go/service/user/admin/rpc/internal/model"
+	"sea-try-go/service/user/admin/rpc/internal/svc"
+	"sea-try-go/service/user/admin/rpc/pb"
+	"sea-try-go/service/user/common/errmsg"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type DeleteUserLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewDeleteUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *DeleteUserLogic {
+	return &DeleteUserLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *DeleteUserLogic) DeleteUser(in *pb.DeleteUserReq) (*pb.DeleteUserResp, error) {
+	err := l.svcCtx.AdminModel.DeleteOneUserByUid(l.ctx, in.Uid)
+	if err != nil {
+		if err == model.ErrorNotFound {
+			return nil, errors.New(errmsg.GetErrMsg(errmsg.ErrorUserNotExist))
+		}
+	}
+	return &pb.DeleteUserResp{
+		Success: true,
+	}, nil
+}

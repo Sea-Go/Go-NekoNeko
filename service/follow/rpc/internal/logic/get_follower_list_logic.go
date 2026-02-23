8296182b9cadf0a6eb6f1b7ceeb7acf5b1b5ef29
夏尾草 <1910541664@qq.com>
2026-02-24 03:00:34 +0800

    follow: add metrics in get_follower_list_logic

--- service/follow/rpc/internal/logic/get_follower_list_logic.go
+++ service/follow/rpc/internal/logic/get_follower_list_logic.go
@@ -2,7 +2,12 @@ package logic
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	"sea-try-go/service/common/logger"
+	"sea-try-go/service/follow/common/errmsg"
+	"sea-try-go/service/follow/rpc/internal/metrics"
 	"sea-try-go/service/follow/rpc/internal/svc"
 	"sea-try-go/service/follow/rpc/pb"
 
@@ -20,10 +25,20 @@ func NewGetFollowerListLogic(ctx context.Context, svcCtx *svc.ServiceContext) *G
 }
 
 func (l *GetFollowerListLogic) GetFollowerList(in *pb.ListReq) (*pb.UserListResp, error) {
+	start := time.Now()
+	resultLabel := "ok"
+	defer func() {
+		metrics.FollowRequestCounterMetric.WithLabelValues("follow_rpc", "GetFollowerList", resultLabel).Inc()
+		metrics.FollowRequestSecondsCounterMetric.WithLabelValues("follow_rpc", "GetFollowerList").Add(time.Since(start).Seconds())
+	}()
+
 	ids, err := l.svcCtx.FollowModel.GetFollowerList(l.ctx, in.UserId, in.Offset, in.Limit)
 	if err != nil {
-		l.Logger.Errorf("GetFollowerList db err: %v", err)
-		return &pb.UserListResp{Code: 500, Msg: "DB Error"}, err
+		resultLabel = "sys_fail"
+		logger.LogBusinessErr(l.ctx, errmsg.ErrorDbRead, fmt.Errorf("GetFollowerList db err: %w", err))
+		return &pb.UserListResp{Code: errmsg.ErrorDbRead, Msg: errmsg.GetErrMsg(errmsg.ErrorDbRead)}, err
 	}
-	return &pb.UserListResp{Code: 0, Msg: "success", UserIds: ids}, nil
+
+	metrics.FollowListSizeGaugeMetric.WithLabelValues("follow_list", "follower").Set(float64(len(ids)))
+	return &pb.UserListResp{Code: errmsg.Success, Msg: errmsg.GetErrMsg(errmsg.Success), UserIds: ids}, nil
 }

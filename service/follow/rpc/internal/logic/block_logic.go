f8c5f145ef9c8595f62da3eccdafa360c6bf75e3
夏尾草 <1910541664@qq.com>
2026-02-24 03:00:33 +0800

    follow: add metrics in block_logic

--- service/follow/rpc/internal/logic/block_logic.go
+++ service/follow/rpc/internal/logic/block_logic.go
@@ -2,7 +2,12 @@ package logic
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	"sea-try-go/service/common/logger"
+	"sea-try-go/service/follow/common/errmsg"
+	"sea-try-go/service/follow/rpc/internal/metrics"
 	"sea-try-go/service/follow/rpc/internal/svc"
 	"sea-try-go/service/follow/rpc/pb"
 
@@ -20,14 +25,26 @@ func NewBlockLogic(ctx context.Context, svcCtx *svc.ServiceContext) *BlockLogic 
 }
 
 func (l *BlockLogic) Block(in *pb.RelationReq) (*pb.BaseResp, error) {
+	start := time.Now()
+	resultLabel := "ok"
+	defer func() {
+		metrics.FollowRequestCounterMetric.WithLabelValues("follow_rpc", "Block", resultLabel).Inc()
+		metrics.FollowRequestSecondsCounterMetric.WithLabelValues("follow_rpc", "Block").Add(time.Since(start).Seconds())
+	}()
+
 	if in.UserId == in.TargetId {
-		return &pb.BaseResp{Code: 400, Msg: "Cannot block yourself"}, nil
+		resultLabel = "biz_fail"
+		metrics.FollowRelationCounterMetric.WithLabelValues("follow_relation", "block", "fail").Inc()
+		return &pb.BaseResp{Code: errmsg.ErrorCannotBlockSelf, Msg: errmsg.GetErrMsg(errmsg.ErrorCannotBlockSelf)}, nil
 	}
 
 	err := l.svcCtx.FollowModel.BlockUser(l.ctx, in.UserId, in.TargetId)
 	if err != nil {
-		l.Logger.Errorf("BlockUser db err: %v", err)
-		return &pb.BaseResp{Code: 500, Msg: "DB Error"}, err
+		resultLabel = "sys_fail"
+		metrics.FollowRelationCounterMetric.WithLabelValues("follow_relation", "block", "fail").Inc()
+		logger.LogBusinessErr(l.ctx, errmsg.ErrorDbWrite, fmt.Errorf("BlockUser db err: %w", err))
+		return &pb.BaseResp{Code: errmsg.ErrorDbWrite, Msg: errmsg.GetErrMsg(errmsg.ErrorDbWrite)}, err
 	}
-	return &pb.BaseResp{Code: 0, Msg: "success"}, nil
+	metrics.FollowRelationCounterMetric.WithLabelValues("follow_relation", "block", "ok").Inc()
+	return &pb.BaseResp{Code: errmsg.Success, Msg: errmsg.GetErrMsg(errmsg.Success)}, nil
 }

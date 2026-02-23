8e0fb23c196054b4dc435f77fba9a656556d4ba9
夏尾草 <1910541664@qq.com>
2026-02-24 03:00:34 +0800

    follow: add metrics in unfollow_logic

--- service/follow/rpc/internal/logic/unfollow_logic.go
+++ service/follow/rpc/internal/logic/unfollow_logic.go
@@ -2,7 +2,12 @@ package logic
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	"sea-try-go/service/common/logger"
+	"sea-try-go/service/follow/common/errmsg"
+	"sea-try-go/service/follow/rpc/internal/metrics"
 	"sea-try-go/service/follow/rpc/internal/svc"
 	"sea-try-go/service/follow/rpc/pb"
 
@@ -20,10 +25,20 @@ func NewUnfollowLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Unfollow
 }
 
 func (l *UnfollowLogic) Unfollow(in *pb.RelationReq) (*pb.BaseResp, error) {
+	start := time.Now()
+	resultLabel := "ok"
+	defer func() {
+		metrics.FollowRequestCounterMetric.WithLabelValues("follow_rpc", "Unfollow", resultLabel).Inc()
+		metrics.FollowRequestSecondsCounterMetric.WithLabelValues("follow_rpc", "Unfollow").Add(time.Since(start).Seconds())
+	}()
+
 	err := l.svcCtx.FollowModel.UnfollowUser(l.ctx, in.UserId, in.TargetId)
 	if err != nil {
-		l.Logger.Errorf("UnfollowUser db err: %v", err)
-		return &pb.BaseResp{Code: 500, Msg: "DB Error"}, err
+		resultLabel = "sys_fail"
+		metrics.FollowRelationCounterMetric.WithLabelValues("follow_relation", "unfollow", "fail").Inc()
+		logger.LogBusinessErr(l.ctx, errmsg.ErrorDbWrite, fmt.Errorf("UnfollowUser db err: %w", err))
+		return &pb.BaseResp{Code: errmsg.ErrorDbWrite, Msg: errmsg.GetErrMsg(errmsg.ErrorDbWrite)}, err
 	}
-	return &pb.BaseResp{Code: 0, Msg: "success"}, nil
+	metrics.FollowRelationCounterMetric.WithLabelValues("follow_relation", "unfollow", "ok").Inc()
+	return &pb.BaseResp{Code: errmsg.Success, Msg: errmsg.GetErrMsg(errmsg.Success)}, nil
 }

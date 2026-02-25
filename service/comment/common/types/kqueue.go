1b72e02ba147ba644cf882a837641d9583f57b7c
unicornshjl <3159669749@qq.com>
2026-02-25 20:35:40 +0800

    feat(comment):定义kafka消息队列传输消息的数据结构类型

---
+++ service/comment/common/types/kqueue.go
@@ -0,0 +1,14 @@
+package kqtypes
+
+type CommentKafkaMsg struct {
+	CommentId  int64  `json:"comment_id"`
+	TargetType string `json:"target_type"`
+	TargetId   string `json:"target_id"`
+	UserId     int64  `json:"user_id"`
+	RootId     int64  `json:"root_id"`
+	ParentId   int64  `json:"parent_id"`
+	Content    string `json:"content"`
+	Meta       string `json:"meta"`
+	Attribute  int64  `json:"attribute"`
+	CreateTime int64  `json:"create_time"`
+}

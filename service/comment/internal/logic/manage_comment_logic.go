feccf6182e6f2b96d7aa1a6d09ece98619fd0d51
unicornshjl <3159669749@qq.com>
2026-02-25 23:03:44 +0800

    feat(comment):新增管理某条评论逻辑(包括但不限于置顶、精选)

--- service/comment/internal/logic/manage_comment_logic.go
+++ service/comment/internal/logic/manage_comment_logic.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"context"
+	"fmt"
 
 	"sea-try-go/service/comment/internal/svc"
 	"sea-try-go/service/comment/pb"
@@ -24,7 +25,30 @@ func NewManageCommentLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Man
 }
 
 func (l *ManageCommentLogic) ManageComment(in *pb.ManageCommentReq) (*pb.ManageCommentResp, error) {
-	// todo: add your logic here and delete this line
-
-	return &pb.ManageCommentResp{}, nil
+	if in.CommentId == 0 {
+		return nil, fmt.Errorf("参数错误:评论ID不能为空")
+	}
+	var bitOffset uint
+	var isSet bool
+	switch in.ActionType {
+	case pb.ManageType_MANAGE_PIN:
+		bitOffset = 2
+		isSet = true
+	case pb.ManageType_MANAGE_UNPIN:
+		bitOffset = 2
+		isSet = false
+	case pb.ManageType_MANAGE_FEATURE:
+		bitOffset = 3
+		isSet = true
+	case pb.ManageType_MANAGE_UNFEATURE:
+		bitOffset = 3
+		isSet = false
+	}
+	err := l.svcCtx.CommentModel.ManageCommentAttribute(l.ctx, in.CommentId, bitOffset, isSet)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.ManageCommentResp{
+		Success: true,
+	}, nil
 }

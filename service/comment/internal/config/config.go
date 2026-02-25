a81e3660d2ac8b43ca31a3e3910fed6aff4f2696
unicornshjl <3159669749@qq.com>
2026-02-25 20:36:37 +0800

    fix(comment):修改了相关的配置文件(Kafka,Redis,PgSql,Etcd)

--- service/comment/internal/config/config.go
+++ service/comment/internal/config/config.go
@@ -1,7 +1,20 @@
 package config
 
-import "github.com/zeromicro/go-zero/zrpc"
+import (
+	"github.com/zeromicro/go-queue/kq"
+	"github.com/zeromicro/go-zero/core/stores/redis"
+	"github.com/zeromicro/go-zero/zrpc"
+)
 
 type Config struct {
 	zrpc.RpcServerConf
+	DB struct {
+		DataSource string
+	}
+	KqPusherConf struct {
+		Brokers []string
+		Topic   string
+	}
+	BizRedis       redis.RedisConf
+	KqConsumerConf kq.KqConf
 }
--- service/comment/internal/svc/service_context.go
+++ service/comment/internal/svc/service_context.go
@@ -1,13 +1,28 @@
 package svc
 
-import "sea-try-go/service/comment/internal/config"
+import (
+	"sea-try-go/service/comment/internal/config"
+	"sea-try-go/service/comment/internal/model"
+	"sea-try-go/service/comment/internal/utils"
+
+	"github.com/zeromicro/go-queue/kq"
+)
 
 type ServiceContext struct {
-	Config config.Config
+	Config          config.Config
+	CommentModel    *model.CommentModel
+	KqPusherClient  *kq.Pusher
+	SensitiveFilter *utils.SensitiveFilter
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	db := model.InitDB(c.DB.DataSource)
+	pusher := kq.NewPusher(c.KqPusherConf.Brokers, c.KqPusherConf.Topic)
+	blackwords := []string{"傻逼", "我草"}
 	return &ServiceContext{
-		Config: c,
+		Config:          c,
+		CommentModel:    model.NewCommentModel(db),
+		KqPusherClient:  pusher,
+		SensitiveFilter: utils.NewSensitiveFilter(blackwords),
 	}
 }

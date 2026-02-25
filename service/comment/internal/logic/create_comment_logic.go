34f8539dfc01520fa6ce1dc1d748e0ed25121cbf
unicornshjl <3159669749@qq.com>
2026-02-25 20:47:42 +0800

    feat(comment):实现评论异步创建逻辑，集成Snowflake以及Kafka发送消息

--- service/comment/internal/logic/create_comment_logic.go
+++ service/comment/internal/logic/create_comment_logic.go
@@ -2,9 +2,14 @@ package logic
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"time"
 
+	kqtypes "sea-try-go/service/comment/common/types"
 	"sea-try-go/service/comment/internal/svc"
 	"sea-try-go/service/comment/pb"
+	"sea-try-go/service/common/snowflake"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -24,7 +29,43 @@ func NewCreateCommentLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Cre
 }
 
 func (l *CreateCommentLogic) CreateComment(in *pb.CreateCommentReq) (*pb.CreateCommentResp, error) {
-	// todo: add your logic here and delete this line
+	if in.TargetId == "" || in.TargetType == "" {
+		return nil, fmt.Errorf("参数错误:类型与ID不能为空")
+	}
+	if in.Content == "" {
+		return nil, fmt.Errorf("参数错误:内容不能为空")
+	}
+	commentId, err := snowflake.GetID()
+	if err != nil {
+		return nil, fmt.Errorf("雪花算法生成ID出错")
+	}
+	now := time.Now()
+	msg := kqtypes.CommentKafkaMsg{
+		CommentId:  commentId,
+		TargetType: in.TargetType,
+		TargetId:   in.TargetId,
+		UserId:     in.UserId,
+		RootId:     in.RootId,
+		ParentId:   in.ParentId,
+		Content:    in.Content,
+		Meta:       in.Meta,
+		Attribute:  0,
+		CreateTime: now.Unix(),
+	}
+	msgBytes, err := json.Marshal(msg)
+	if err != nil {
+		return nil, fmt.Errorf("Kafka消息序列化失败:%v", err)
+	}
+	err = l.svcCtx.KqPusherClient.Push(l.ctx, string(msgBytes))
+	if err != nil {
+		return nil, fmt.Errorf("Kafka推送消息失败:%v", err)
+	}
 
-	return &pb.CreateCommentResp{}, nil
+	return &pb.CreateCommentResp{
+		Id: commentId,
+		//将时间格式化为"2006-01-02 15:04:05"
+		CreatedAt: now.Format(time.DateTime),
+		//Gemini说返回0,"客户端乐观更新"
+		SubjectCount: 0,
+	}, nil
 }

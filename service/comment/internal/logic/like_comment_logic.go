e84a0e248ab03c752e6a7e49a9594301f0cf81e1
unicornshjl <3159669749@qq.com>
2026-02-27 22:28:21 +0800

    feat(comment):新增给某条评论点赞的逻辑

--- service/comment/internal/logic/like_comment_logic.go
+++ service/comment/internal/logic/like_comment_logic.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"context"
+	"fmt"
 
 	"sea-try-go/service/comment/internal/svc"
 	"sea-try-go/service/comment/pb"
@@ -24,7 +25,19 @@ func NewLikeCommentLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LikeC
 }
 
 func (l *LikeCommentLogic) LikeComment(in *pb.LikeCommentReq) (*pb.LikeCommentResp, error) {
-	// todo: add your logic here and delete this line
+	if in.CommentId == 0 {
+		return nil, fmt.Errorf("参数错误:评论ID不能为空")
+	}
+	ownerId, err := l.svcCtx.CommentModel.GetOwnerId(l.ctx, in.TargetType, in.TargetId)
+	if err != nil {
+		return nil, fmt.Errorf("查询Owner失败")
+	}
+	err = l.svcCtx.CommentModel.LikeCommentTx(l.ctx, in.UserId, in.CommentId, in.TargetType, in.TargetId, in.ActionType, int64(ownerId))
+	if err != nil {
+		return nil, err
+	}
 
-	return &pb.LikeCommentResp{}, nil
+	return &pb.LikeCommentResp{
+		Success: true,
+	}, nil
 }

54790c067377de86b6d49aa4c07ddec5beaaa576
senak1129 <1418172577@qq.com>
2026-02-28 16:49:37 +0800

    4

--- service/article/rpc/internal/logic/delete_article_logic.go
+++ service/article/rpc/internal/logic/delete_article_logic.go
@@ -1,18 +1,15 @@
 package logic
 
 import (
-```
 	"context"
-	"fmt" // Added for fmt.Errorf
+	"fmt"
 
 	"sea-try-go/service/article/rpc/internal/svc"
 	"sea-try-go/service/article/rpc/pb"
 
+	"github.com/minio/minio-go/v7"
 	"github.com/zeromicro/go-zero/core/logx"
-	"github.com/minio/minio-go/v7" // 引入 minio 包
 )
-
-```
 
 type DeleteArticleLogic struct {
 	ctx    context.Context
@@ -29,7 +26,6 @@ func NewDeleteArticleLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Del
 }
 
 func (l *DeleteArticleLogic) DeleteArticle(in *__.DeleteArticleRequest) (*__.DeleteArticleResponse, error) {
-```
 	article, err := l.svcCtx.ArticleRepo.FindOne(l.ctx, in.ArticleId)
 	if err != nil {
 		l.Logger.Errorf("DeleteArticle FindOne error: %v", err)
@@ -39,24 +35,19 @@ func (l *DeleteArticleLogic) DeleteArticle(in *__.DeleteArticleRequest) (*__.Del
 		return nil, fmt.Errorf("article not found")
 	}
 
-	// 1. 从 MinIO 删除对应的 Markdown 内容
-	if article.Content != "" { // 确保有内容路径才删除
+	if article.Content != "" {
 		err = l.svcCtx.MinioClient.RemoveObject(l.ctx, l.svcCtx.Config.MinIO.BucketName, article.Content, minio.RemoveObjectOptions{})
 		if err != nil {
 			l.Logger.Errorf("DeleteArticle RemoveObject from MinIO error: %v, object: %s", err, article.Content)
-			// 这里可以选择是否中断删除操作。通常，即使 MinIO 删除失败，数据库记录也应该被删除。
-			// 但为了数据一致性，这里选择返回错误，让用户重试或手动处理。
 			return nil, err
 		}
 	}
 
-	// 2. 删除数据库记录
 	if err := l.svcCtx.ArticleRepo.Delete(l.ctx, in.ArticleId); err != nil {
 		l.Logger.Errorf("DeleteArticle Delete error: %v", err)
 		return nil, err
 	}
 
-```
 	return &__.DeleteArticleResponse{
 		Success: true,
 	}, nil
